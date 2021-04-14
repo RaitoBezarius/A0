6d397b4a341cac859790de2cd1af4af764ee63f3
Raito Bezarius <masterancpp@gmail.com>
2021-04-15 01:23:30 +0200

    scripts: enter qemu monitor mode script

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 {
   shell = pkgs.mkShell {
-    buildInputs = with pkgs; [ zig qemu lldb gdb ];
+    buildInputs = with pkgs; [ zig qemu gdb socat (writeScriptBin "enter-qemu-monitor" ''
+      #!${stdenv.shell}
+      socat -,echo=0,icanon=0 unix-connect:qemu-monitor-socket
+    '')];
     OVMF_FW_CODE_PATH = "${pkgs.OVMF.fd}/FV/OVMF_CODE.fd";
   };
 }
