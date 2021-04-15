a245a2f86c59691c8529c5eb63fefc9544707c1e
Raito Bezarius <masterancpp@gmail.com>
2021-04-15 04:37:25 +0200

    isr: make it 64 bits and use proper flags for gate installations

--- src/kernel/arch/x86/isr.s
+++ src/kernel/arch/x86/isr.s
@@ -26,10 +26,12 @@ isrCommon:
     push %rax
     push %rcx
     push %rdx
-    push %rbx
-    push %rbp
     push %rsi
     push %rdi
+    push %r8
+    push %r9
+    push %r10
+    push %r11
 
     // Setup kernel data segment.
     // mov $KERNEL_DS, %ax
@@ -50,10 +52,12 @@ isrCommon:
     // mov %ax, %ds
     // mov %ax, %es
 
+    pop %r11
+    pop %r10
+    pop %r9
+    pop %r8
     pop %rdi
     pop %rsi
-    pop %rbp
-    pop %rbx
     pop %rdx
     pop %rcx
     pop %rax
--- src/kernel/arch/x86/isr.zig
+++ src/kernel/arch/x86/isr.zig
@@ -63,7 +63,7 @@ pub const Context = packed struct {
     cs: u64,
     rflags: u64,
     rsp: u64,
-    ss: u32,
+    ss: u64,
 
     pub fn setReturnValue(self: *volatile Context, value: anytype) void {
         self.registers.rax = if (@TypeOf(value) == bool) @boolToInt(value) else @as(u64, value);
@@ -71,9 +71,7 @@ pub const Context = packed struct {
 };
 
 // Structure holding general purpose registers as saved by isrCommon.
-pub const Registers = packed struct {
-    rax: u64, rcx: u64, rdx: u64, rsi: u64, rdi: u64, r8: u64, r9: u64, r10: u64, r11: u64
-};
+pub const Registers = packed struct { rax: u64, rcx: u64, rdx: u64, rsi: u64, rdi: u64, r8: u64, r9: u64, r10: u64, r11: u64 };
 
 // Pointer to the current saved context.
 pub export var context: *volatile Context = undefined;
@@ -82,57 +80,57 @@ pub export var context: *volatile Context = undefined;
 //
 pub fn install_exceptions() void {
     // Exceptions.
-    idt.setGate(1, idt.INTERRUPT_GATE, isr1);
-    idt.setGate(2, idt.INTERRUPT_GATE, isr2);
-    idt.setGate(3, idt.INTERRUPT_GATE, isr3);
-    idt.setGate(4, idt.INTERRUPT_GATE, isr4);
-    idt.setGate(5, idt.INTERRUPT_GATE, isr5);
-    idt.setGate(6, idt.INTERRUPT_GATE, isr6);
-    idt.setGate(7, idt.INTERRUPT_GATE, isr7);
-    idt.setGate(8, idt.INTERRUPT_GATE, isr8);
-    idt.setGate(9, idt.INTERRUPT_GATE, isr9);
-    idt.setGate(10, idt.INTERRUPT_GATE, isr10);
-    idt.setGate(11, idt.INTERRUPT_GATE, isr11);
-    idt.setGate(12, idt.INTERRUPT_GATE, isr12);
-    idt.setGate(13, idt.INTERRUPT_GATE, isr13);
-    idt.setGate(14, idt.INTERRUPT_GATE, isr14);
-    idt.setGate(15, idt.INTERRUPT_GATE, isr15);
-    idt.setGate(16, idt.INTERRUPT_GATE, isr16);
-    idt.setGate(17, idt.INTERRUPT_GATE, isr17);
-    idt.setGate(18, idt.INTERRUPT_GATE, isr18);
-    idt.setGate(19, idt.INTERRUPT_GATE, isr19);
-    idt.setGate(20, idt.INTERRUPT_GATE, isr20);
-    idt.setGate(21, idt.INTERRUPT_GATE, isr21);
-    idt.setGate(22, idt.INTERRUPT_GATE, isr22);
-    idt.setGate(23, idt.INTERRUPT_GATE, isr23);
-    idt.setGate(24, idt.INTERRUPT_GATE, isr24);
-    idt.setGate(25, idt.INTERRUPT_GATE, isr25);
-    idt.setGate(26, idt.INTERRUPT_GATE, isr26);
-    idt.setGate(27, idt.INTERRUPT_GATE, isr27);
-    idt.setGate(28, idt.INTERRUPT_GATE, isr28);
-    idt.setGate(29, idt.INTERRUPT_GATE, isr29);
-    idt.setGate(30, idt.INTERRUPT_GATE, isr30);
-    idt.setGate(31, idt.INTERRUPT_GATE, isr31);
+    idt.setGate(1, idt.InterruptGateFlags, isr1);
+    idt.setGate(2, idt.InterruptGateFlags, isr2);
+    idt.setGate(3, idt.InterruptGateFlags, isr3);
+    idt.setGate(4, idt.InterruptGateFlags, isr4);
+    idt.setGate(5, idt.InterruptGateFlags, isr5);
+    idt.setGate(6, idt.InterruptGateFlags, isr6);
+    idt.setGate(7, idt.InterruptGateFlags, isr7);
+    idt.setGate(8, idt.InterruptGateFlags, isr8);
+    idt.setGate(9, idt.InterruptGateFlags, isr9);
+    idt.setGate(10, idt.InterruptGateFlags, isr10);
+    idt.setGate(11, idt.InterruptGateFlags, isr11);
+    idt.setGate(12, idt.InterruptGateFlags, isr12);
+    idt.setGate(13, idt.InterruptGateFlags, isr13);
+    idt.setGate(14, idt.InterruptGateFlags, isr14);
+    idt.setGate(15, idt.InterruptGateFlags, isr15);
+    idt.setGate(16, idt.InterruptGateFlags, isr16);
+    idt.setGate(17, idt.InterruptGateFlags, isr17);
+    idt.setGate(18, idt.InterruptGateFlags, isr18);
+    idt.setGate(19, idt.InterruptGateFlags, isr19);
+    idt.setGate(20, idt.InterruptGateFlags, isr20);
+    idt.setGate(21, idt.InterruptGateFlags, isr21);
+    idt.setGate(22, idt.InterruptGateFlags, isr22);
+    idt.setGate(23, idt.InterruptGateFlags, isr23);
+    idt.setGate(24, idt.InterruptGateFlags, isr24);
+    idt.setGate(25, idt.InterruptGateFlags, isr25);
+    idt.setGate(26, idt.InterruptGateFlags, isr26);
+    idt.setGate(27, idt.InterruptGateFlags, isr27);
+    idt.setGate(28, idt.InterruptGateFlags, isr28);
+    idt.setGate(29, idt.InterruptGateFlags, isr29);
+    idt.setGate(30, idt.InterruptGateFlags, isr30);
+    idt.setGate(31, idt.InterruptGateFlags, isr31);
 }
 
 // IRQs.
 pub fn install_irqs() void {
-    idt.setGate(32, idt.INTERRUPT_GATE, isr32);
-    idt.setGate(33, idt.INTERRUPT_GATE, isr33);
-    idt.setGate(34, idt.INTERRUPT_GATE, isr34);
-    idt.setGate(35, idt.INTERRUPT_GATE, isr35);
-    idt.setGate(36, idt.INTERRUPT_GATE, isr36);
-    idt.setGate(37, idt.INTERRUPT_GATE, isr37);
-    idt.setGate(38, idt.INTERRUPT_GATE, isr38);
-    idt.setGate(39, idt.INTERRUPT_GATE, isr39);
-    idt.setGate(40, idt.INTERRUPT_GATE, isr40);
-    idt.setGate(41, idt.INTERRUPT_GATE, isr41);
-    idt.setGate(42, idt.INTERRUPT_GATE, isr42);
-    idt.setGate(43, idt.INTERRUPT_GATE, isr43);
-    idt.setGate(44, idt.INTERRUPT_GATE, isr44);
-    idt.setGate(45, idt.INTERRUPT_GATE, isr45);
-    idt.setGate(46, idt.INTERRUPT_GATE, isr46);
-    idt.setGate(47, idt.INTERRUPT_GATE, isr47);
+    idt.setGate(32, idt.InterruptGateFlags, isr32);
+    idt.setGate(33, idt.InterruptGateFlags, isr33);
+    idt.setGate(34, idt.InterruptGateFlags, isr34);
+    idt.setGate(35, idt.InterruptGateFlags, isr35);
+    idt.setGate(36, idt.InterruptGateFlags, isr36);
+    idt.setGate(37, idt.InterruptGateFlags, isr37);
+    idt.setGate(38, idt.InterruptGateFlags, isr38);
+    idt.setGate(39, idt.InterruptGateFlags, isr39);
+    idt.setGate(40, idt.InterruptGateFlags, isr40);
+    idt.setGate(41, idt.InterruptGateFlags, isr41);
+    idt.setGate(42, idt.InterruptGateFlags, isr42);
+    idt.setGate(43, idt.InterruptGateFlags, isr43);
+    idt.setGate(44, idt.InterruptGateFlags, isr44);
+    idt.setGate(45, idt.InterruptGateFlags, isr45);
+    idt.setGate(46, idt.InterruptGateFlags, isr46);
+    idt.setGate(47, idt.InterruptGateFlags, isr47);
 }
 
 // Syscalls.
